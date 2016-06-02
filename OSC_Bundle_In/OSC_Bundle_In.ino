ec83e880046a2d84f166037620d6694a526a1229
videojedi <rburford@me.com>
2016-06-02 14:56:35 +0100

    made it work!

--- OSC_Bundle_In/OSC_Bundle_In.ino
+++ OSC_Bundle_In/OSC_Bundle_In.ino
@@ -11,16 +11,21 @@ byte mac[] = {
 
 // NOTE: Alternatively, you can assign a fixed IP to configure your
 //       Ethernet shield.
-      byte ip[] = { 192, 100, 100, 177 };
+      byte ip[] = { 192, 168, 000, 177 };
+      byte e2IP[] = {192, 168, 000, 175 };
+      
 
 
 int serverPort  = 8000; //TouchOSC (incoming port)
-int destPort = 8001;    //TouchOSC (outgoing port)
+int e2port = 9878;    //TouchOSC (outgoing port)
 int ledPin =  13;       //pin 13 on Arduino Uno. Pin 6 on a Teensy++2
 int ledState = LOW;
+float value = 0;
+String inData;
 
 //Create UDP message object
 EthernetUDP Udp;
+EthernetClient client;
 
 void setup(){
   Serial.begin(9600); //9600 for a "normal" Arduino board (Uno for example). 115200 for a Teensy ++2 
@@ -40,12 +45,18 @@ Ethernet.begin(mac, ip);
   for (byte thisByte = 0; thisByte < 4; thisByte++) {
     // print the value of each byte of the IP address:
     Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");  
+    }
+    Serial.println("");
+
+if (client.connect(e2IP,e2port)) {
+  
+Serial.println("Connected");
      
-  }
+}
 
   Udp.begin(serverPort);
 }
-
 void loop(){
   //process received messages
   OSCMsgReceive();
@@ -55,57 +66,89 @@ void OSCMsgReceive(){
   OSCBundle msgIN;
   int size;
   if((size = Udp.parsePacket())>0){
-    //Serial.println("Message...");
+    
     while(size--)
       msgIN.fill(Udp.read());
 
     if(!msgIN.hasError()){
-      msgIN.route("/cue",toggleOnOff);
+      
+      msgIN.route("/cue",preset);
       msgIN.route("/test1",funcValue);
     }
     else Serial.println("Error...");
   }
 }
 
-void toggleOnOff(OSCMessage &msg, int addrOffset){
-  Serial.println("/test");
-  ledState = (boolean) msg.getFloat(0);
-  OSCMessage msgOUT("/OnOff/toggle1");
-
-  digitalWrite(ledPin, ledState);
-
-  msgOUT.add(ledState);
-  if (ledState) {
-    Serial.println("LED on");
-  }
-  else {
-    Serial.println("LED off");
-  }
-
-  ledState = !ledState;     // toggle the state from HIGH to LOW to HIGH to LOW ...
-
-  //send osc message back to controll object in TouchOSC
-  //Local feedback is turned off in the TouchOSC interface.
-  //The button is turned on in TouchOSC interface whe the conrol receives this message.
-  Udp.beginPacket(Udp.remoteIP(), destPort);
-  msgOUT.send(Udp); // send the bytes
-  Udp.endPacket(); // mark the end of the OSC Packet
-  msgOUT.empty(); // free space occupied by message
+void preset(OSCMessage &msg, int addrOffset){
+  Serial.println("/cue");
+  float value = msg.getFloat(0);
+  //OSCMessage msgOUT("/OnOff/toggle1");
+if (client.connected()) {
+  client.print("preset -r ");
+  Serial.print("preset -r ");
+    client.println(value);
+    Serial.println(value);
+    delay(1000);
+    if (ackn() == 0) {
+      Serial.println("Success!");
+      ATRN();
+    }
+      else Serial.println("Failed!");
+    }
 }
+
+
+
+
 
 void funcValue(OSCMessage &msg, int addrOffset ){
   Serial.println("Test1");
   float value = msg.getFloat(0);
-  OSCMessage msgOUT("/Fader/Value");
+  //OSCMessage msgOUT("/Fader/Value");
 
   Serial.print("Value = : ");
   Serial.println(value);
 
-  msgOUT.add(value);
-
-  Udp.beginPacket(Udp.remoteIP(), destPort);
-  msgOUT.send(Udp); // send the bytes
-  Udp.endPacket(); // mark the end of the OSC Packet
-  msgOUT.empty(); // free space occupied by message
 }
 
+void ATRN() {
+
+  if (client.connected()) {
+    client.println("atrn 60");
+    Serial.println("atrn 60");
+
+
+  }
+
+}
+
+
+int ackn() {
+
+  inData = "";    //Clear buffer
+  Serial.println("ack?");
+  int error;
+
+  while (client.available()) {
+    char c = client.read();
+    inData += c;
+    if (inData.endsWith("-e "))
+    {
+      client.setTimeout(100);
+      error = client.parseInt();
+      Serial.print("Error:");
+      Serial.println(error);
+      break;
+    }
+
+    else if (c == '\n')
+    {
+
+      Serial.print("Recieved:");
+      Serial.println(inData);
+      inData = "";    //Clear buffer
+    }
+  }
+  inData = "";
+  return error;
+}
